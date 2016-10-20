436a8356ae0e78c9dd0e396de74f835fffabb896
Nathan Friedly <nathan@nfriedly.com>
2016-10-20 11:53:38 -0400

    Simplifying upload button test

--- test/integration/test.mainpage.js
+++ test/integration/test.mainpage.js
@@ -48,7 +48,7 @@ casper.test.begin('Document Conversion Demo', 31, function suite(test) {
       this.click('label[for="source-custom"]');
     });
 
-    casper.waitForSelector('div._content--upload', function () {
+    casper.then(function () {
       test.assertExists('label[for="file-chooser-input"]', 'Choose your file - button is found');
     });
   }
